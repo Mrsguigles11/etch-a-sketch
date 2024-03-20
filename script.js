d830691534765f14a29059511c685b6994454e39
Harry Woodgate <harry_woodgate11@hotmail.co.uk>
2024-03-20 16:46:03 +0000

    Edit the JS and CSS files so that the created elements will form a grid in container.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const container = document.querySelector('.container');
+const grid = document.querySelector('.grid');
 
 let squares = 16;
+let squaresTotal = (squares * squares);
 
 function createSquare(num) {
     for (let i = 0; i < num; i++) {
         let div = document.createElement("div");
-        container.appendChild(div);
+        div.setAttribute("id", "square");
+        div.style.width = (800 / squares) + 'px';
+        grid.appendChild(div);
     }
 }
 
-createSquare(squares);
+createSquare(squaresTotal);
+
 
