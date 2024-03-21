be0e7e9d59060136b45973e796fea61343f040f5
Harry Woodgate <harry_woodgate11@hotmail.co.uk>
2024-03-21 16:54:36 +0000

    Finish functionality for the newGrid button so it will create new grid based on user input, and squares will turn black when hovered.

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
 const grid = document.querySelector('.grid');
 const container = document.querySelector('.container');
 
-let squaresRoot = 4;
+let squaresRoot = 10;
 let squaresTotal = (squaresRoot * squaresRoot);
 
 function createSquare(num) {
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i < squaresTotal; i++) {
         let div = document.createElement("div");
         div.setAttribute("id", "square");
-        div.style.width = (700 / squaresRoot) + 'px';
+        div.style.width = (700 / num) + 'px';
         grid.appendChild(div);
     }
 }
 
-createSquare(squaresTotal);
+createSquare(squaresRoot);
 
 let squares = document.querySelectorAll('#square');
 
-for (const square of squares) {
-square.addEventListener('mouseover', () => {
-    square.style.backgroundColor = "black";
-})}
+function squareHover() {
+    for (const square of squares) {
+        square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = "black";
+})} }
+
+squareHover();
 
 
 let newGrid = document.querySelector(".newGrid");
 
 newGrid.addEventListener('click', () => {
     let userNumber = Number(prompt("Choose a number to set the size of the new grid!"));
-    console.log(userNumber);
+    while (grid.firstChild) {
+        grid.removeChild(grid.lastChild);
+    };
+    squaresTotal = (userNumber * userNumber);
+    createSquare(userNumber);
+    squares = document.querySelectorAll('#square');
+    squareHover();
 })
