05e1f97935c9f981b929bb7b269ba6e8203267b2
Harry Woodgate <harry_woodgate11@hotmail.co.uk>
2024-03-21 16:01:34 +0000

    Add event listener to squares so that they turn black when hovered on.

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const grid = document.querySelector('.grid');
 
-let squares = 16;
-let squaresTotal = (squares * squares);
+let squaresRoot = 4;
+let squaresTotal = (squaresRoot * squaresRoot);
 
 function createSquare(num) {
     for (let i = 0; i < num; i++) {
         let div = document.createElement("div");
         div.setAttribute("id", "square");
-        div.style.width = (800 / squares) + 'px';
+        div.style.width = (800 / squaresRoot) + 'px';
         grid.appendChild(div);
     }
 }
 
 createSquare(squaresTotal);
 
+let squares = document.querySelectorAll('#square');
 
+for (const square of squares) {
+square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = "black";
+})}
